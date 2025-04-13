18057600033c1c8e20f35fd4a5c00b645fb71703
Dawit <tesfayedawit22090582@gmail.com>
2025-04-13 23:07:56 +0300

    section2 added

---
+++ src/components/Section2.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Section2() {
+  return (
+    <div>
+      <section className="section2 p-5">
+        <div className="descriptions container text-center text-white">
+          <h1>iPhone 16</h1>
+          <h6>Hello, Apple Intelligence.</h6>
+          <div className="links mt-3">
+            <button className="btn btn-light rounded-pill px-3" type="button">
+              Learn more
+            </button>
+            <button
+              type="button"
+              className="btn btn-outline-light rounded-pill px-3"
+            >
+              Buy
+            </button>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Section2;
