1e6bb3889f7fbe9512b9332bdb5c3fc342cad164
Dawit <tesfayedawit22090582@gmail.com>
2025-04-13 23:27:49 +0300

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Section1 from "./components/Section1";
 import Section2 from "./components/Section2";
 import Section3 from "./components/Section3";
 import Cards from "./components/Cards";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
       <Section1 />
       <Section2 />
       <Section3 />
-      <Cards />
+      <Footer />
     </div>
   );
 }
